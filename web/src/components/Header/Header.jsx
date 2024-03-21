29cd455a95ed8057f644095497c79595c7b4b228
aleks-123 <ace.ki@hotmail.com>
2024-03-21 01:46:06 +0100

    Adding header info

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -4,7 +4,8 @@ import styles from './header.module.css';
 function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>My Notes</h1>
+      <h1 className={styles.title}>AppGreat - Notes Assigment</h1>
+      <h2 className={styles.subTitle}>Made by: Aleksandar Milosheski</h2>
     </header>
   );
 }
